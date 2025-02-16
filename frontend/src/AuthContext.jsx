56ff2148e7dd29fbebc3c3b4b7930db1512297f6
amarjit0786 <amrjitrai232@gmail.com>
2025-02-16 21:11:21 +0530

    working On The Frontend Router

--- frontend/src/AuthContext.jsx
+++ frontend/src/AuthContext.jsx
@@ -1,27 +1,18 @@
-import React, {
-  createContext,
-  useContext,
-  useEffect,
-  useState,
-} from "react";
+import React, { useState, useEffect, createContext, useContext } from "react";
 
 const AuthContext = createContext();
 
 export const useAuth = () => useContext(AuthContext);
-
-export const AuthProvider = ({ children }) => {
+export const AuthProvider = ({ childern }) => {
   const [currentUser, setCurrentUser] = useState(null);
+
   useEffect(() => {
-    // check if user is logged in
+    //check if user is logged in
     const userId = localStorage.getItem("userId");
     if (userId) {
       setCurrentUser(userId);
     }
   }, []);
-
-  const value = {
-    currentUser,
-    setCurrentUser,
-  };
-  return <AuthContext.Provider value={value} >{children}</AuthContext.Provider>
+  const value = { currentUser, setCurrentUser };
+  return <AuthContext.Provider value={value}>{childern}</AuthContext.Provider>;
 };
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { AuthProvider } from './AuthContext.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { AuthProvider } from "./AuthContext.jsx";
+import ProjectRoutes from "./Routes.jsx";
+import { BrowserRouter as Routes } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
- <AuthProvider>
-  <App />
- </AuthProvider>
-)
+createRoot(document.getElementById("root")).render(
+  <AuthProvider>
+    <Routes>
+      <ProjectRoutes />
+    </Routes>
+  </AuthProvider>
+);
