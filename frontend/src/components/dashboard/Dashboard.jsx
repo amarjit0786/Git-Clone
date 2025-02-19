f3eae757e015e4727f82f44600f1d7063bd513c8
amarjit0786 <amrjitrai232@gmail.com>
2025-02-19 22:35:55 +0530

    Building-Dashboard-1

--- frontend/src/components/dashboard/Dashboard.jsx
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -1,6 +1,45 @@
-import React from "react";
+import React ,{useState,useEffect} from "react";
+
+
 
 export default function Dashboard() {
+
+  const [repositories,setRepositories]=useState([]);
+  const [searchQuery,setSearchQuery]=useState("");
+  const [suggestedRepositories,setSuggestedRepositories]=useState([]); 
+  const [searchResults,setSearchResults]=useState("");
+
+  useEffect(()=>{
+    const userId = localStorage.getItem("userId");
+
+    const fetchRepositories = async ()=>{
+      try {
+        const response = await fetch(`http://localhost:3000/repo/user/${userId}`);
+          const data = await response.json();
+          setRepositories(data.repositories);
+        
+      } catch (error) {
+        console.error("error while fetching repositories", error);
+      }  
+    };
+    const fetchSuggestedRepositories = async ()=>{
+      try {
+        const response = await fetch(`http://localhost:3000/repo/all`);
+          const data = await response.json();
+          // console.log(data)
+          setSuggestedRepositories(data);
+          console.log(suggestedRepositories);
+        
+      } catch (error) {
+        console.error("error while fetching repositories", error);
+      }  
+    };
+
+    fetchRepositories();
+    fetchSuggestedRepositories();
+  },[]);
+
+
   return (
     <div>
       <h1>Dashboard</h1>
