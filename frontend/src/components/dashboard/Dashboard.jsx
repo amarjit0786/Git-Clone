0ffe8e5744b04b9e0d054290b8a3c4c9b0df3ed8
amarjit0786 <amrjitrai232@gmail.com>
2025-02-20 11:45:18 +0530

    Building-Dashboard-2

--- frontend/src/components/dashboard/Dashboard.jsx
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -1,48 +1,89 @@
-import React ,{useState,useEffect} from "react";
-
-
+import React, { useState, useEffect } from "react";
+import "./dashboard.css"
 
 export default function Dashboard() {
+  const [repositories, setRepositories] = useState([]);
+  const [suggestedRepositories, setSuggestedRepositories] = useState([]);
 
-  const [repositories,setRepositories]=useState([]);
-  const [searchQuery,setSearchQuery]=useState("");
-  const [suggestedRepositories,setSuggestedRepositories]=useState([]); 
-  const [searchResults,setSearchResults]=useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResults, setSearchResults] = useState("");
 
-  useEffect(()=>{
+  useEffect(() => {
     const userId = localStorage.getItem("userId");
 
-    const fetchRepositories = async ()=>{
+    const fetchRepositories = async () => {
       try {
-        const response = await fetch(`http://localhost:3000/repo/user/${userId}`);
-          const data = await response.json();
-          setRepositories(data.repositories);
-        
+        const response = await fetch(
+          `http://localhost:3000/repo/user/${userId}`
+        );
+        const data = await response.json();
+        setRepositories(data.repositories);
       } catch (error) {
         console.error("error while fetching repositories", error);
-      }  
+      }
     };
-    const fetchSuggestedRepositories = async ()=>{
+    const fetchSuggestedRepositories = async () => {
       try {
         const response = await fetch(`http://localhost:3000/repo/all`);
-          const data = await response.json();
-          // console.log(data)
-          setSuggestedRepositories(data);
-          console.log(suggestedRepositories);
-        
+        const data = await response.json();
+        // console.log(data)
+        setSuggestedRepositories(data);
+        console.log(suggestedRepositories);
       } catch (error) {
         console.error("error while fetching repositories", error);
-      }  
+      }
     };
 
     fetchRepositories();
     fetchSuggestedRepositories();
-  },[]);
+  }, []);
 
+  useEffect(() => {
+    if (searchQuery === "") {
+      setSearchResults(repositories);
+    } else {
+      const filteredRepo = repositories.filter((repo) => {
+        repo.name.toLowerCase().includes(searchQuery.toLowerCase());
+      });
+
+      setSearchResults(filteredRepo);
+    }
+  }, [searchQuery, repositories]);
 
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+   <section id="dashboard">
+    <aside>
+      {/* all repos */}
+      <h2>Suggested Repositories</h2>
+      {suggestedRepositories.map((repo)=>{
+        return (
+          <div key={repo._id}>
+            <h4>{repo.name}</h4>
+            <p>{repo.description}</p>
+          </div>
+        )
+      })}
+    </aside>
+    <main>
+      <h2>Your Repositories</h2>
+      {repositories.map((repo)=>{
+        return (
+          <div key={repo._id}>
+            <h4>{repo.name}</h4>
+            <h4>{repo.description}</h4>
+          </div>
+        )
+      })}
+    </main>
+    <aside>
+      <h3>Upcoming Events</h3>
+      <ul>
+        <li><p>Tech Conference -Dec 15</p></li>
+        <li><p>Tech Conference -Dec 15</p></li>
+        <li><p>Tech Conference -Dec 15</p></li>
+        <li><p>Tech Conference -Dec 15</p></li>
+      </ul>
+    </aside>
+   </section>
   );
 }
